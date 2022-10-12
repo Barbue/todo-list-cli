81bb5cf0f088531e9ad064e85dc2e6c75735ec72
Barbue <david.crochet@codeimmersives.com>
2022-10-11 20:27:51 -0500

    update main.js file

--- main.js
+++ main.js
@@ -2,12 +2,8 @@ const prompt = require('prompt-sync')({sigint: true});
 
 console.log("Welcome to the To-Do List Manager Application!");
 
-console.log("~ Select an action ~");
-console.log("[1] Create a to-do item");
-console.log("[2] Complete a to-do item");
-console.log("[3] Exit To-do list Application");
 
-let option = Number(prompt("> "));
+let option = selectOption();
 let toDoList = [];
 let statusArray = [];
 
@@ -103,7 +99,6 @@ console.log(`${i + 1}. ${status} ${toDoList[i]}`);
 
 
 /*
-
 1.Figure out the UI
    -console.log()
   -welcome message
@@ -115,7 +110,6 @@ console.log(`${i + 1}. ${status} ${toDoList[i]}`);
   1 adding a task/create an item - prompt user for item to create
   2 mark task as complete - prompt user for which task is complete
   3 exit
-
   - while loop
   program has no known ending, so we want to be able to prompt the indefinitely/until they decide to exit
   -display the to-do list to the user
@@ -123,44 +117,25 @@ console.log(`${i + 1}. ${status} ${toDoList[i]}`);
     -the name of the item
     -the number associated with the item
     -loop to display each item in the todo list
-
-
-
-
-
 2.Add to do items (choice === 1)
 -prompt for the to-do item
 -save the item by storing it in an array - .push()
 - an array to keep track of toDo items -global variable(can access from anywhere in code)
-
-
 2.5 - how do we set items to be incomplete
 //any item in the list is [incomplete] until we mark it [complete]
 - whenever we add an item to the to do list it is [imcomplete] by default
-
 array of booleans 
 [ true,       false,     true] status array
 ["Walk Dog", "Get food","Run mile"]toDoList array shared index
   0           1          2  shared index
-
   true = [complete]
   false = [incomplete]
-
   let status = [];
   by default, when we add a new item, we want to add a false boolean to our status array.
-
-
-
 3.complete to do items
 -display current items in the toDo list w/ their status
 -pick which toDo item to mark as complete - swap its status from false to true
 -make sure we display the updated list right after
-
-
-
-
-
-
-
-
 */
+
+
